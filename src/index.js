f129d1a86caf88480f752bbfc21bd43cacf2aa63
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-03-12 11:02:10 +0500

    devtool added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { QueryClientProvider, QueryClient } from "react-query";
-
+import { ReactQueryDevtools } from "react-query/devtools";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>
 );
