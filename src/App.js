7a6e38e4c2d6399d1cb99af9a6d48f7d266a8b3b
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-03-12 17:39:10 +0500

    data transformation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 const App = () => {
+  const onSuccess = (data) => console.log("Perform after success", data);
+  const onError = (error) => console.log("Perform after success", error);
   const getData = () => axios.get('http://localhost:4000/api/projects');
-  const { isLoading, data, isError, error } = useQuery('api-project', getData);
+  const { isLoading, data, isError, error } = useQuery('api-project', getData, {
+    onSuccess,
+    onError,
+    select: (data) => data.data.data.map(project => project.name)
+
+  });
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
   if (isError) {
     return <h2>{error.message}</h2>;
   }
+  console.log(data);
   return (
     <>
-      {data.data.data?.map((values, index) => {
-        return <div key={index}>
-          <h2>{values.name}</h2>
+      {data?.map((projectName) => {
+        return <div key={projectName}>
+          <h2>{projectName}</h2>
         </div>;
       })
       }
