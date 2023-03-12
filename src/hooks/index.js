73c66758be838fe7c6ae92a91d9bc4793a9193f8
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-03-12 18:00:16 +0500

    Update index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,11 +2,15 @@ import axios from "axios";
 import { useQuery } from "react-query";
 const BASE_URL = "http://localhost:4000/api";
 const getData = () => axios.get(`${BASE_URL}/projects`);
-export const useFetchAPI = (onSuccess, onError) => {
+const useFetchAPI = (onSuccess, onError) => {
  return useQuery('api-project', getData, {
   onSuccess,
   onError,
   select: (data) => data.data.data.map(project => project.name)
 
  });
+};
+
+export {
+ useFetchAPI
 };
